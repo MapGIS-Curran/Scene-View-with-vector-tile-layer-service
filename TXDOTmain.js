8d408bd2d0f7e2dbd648acb5aec69cb83c44de40
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-26 01:57:40 -0900

    Update TXDOTmain.js

--- TXDOTmain.js
+++ TXDOTmain.js
@@ -1,19 +1,22 @@
-      require([
-        "esri/Map", "esri/views/SceneView", "esri/layers/VectorTileLayer"], function(Map, SceneView, VectorTileLayer) {
-        // Create a Map
-        var map = new Map();
+require([
+  "esri/Map",
+  "esri/views/SceneView",
+  "esri/layers/VectorTileLayer"
+], function(Map, SceneView, VectorTileLayer) {
+  // Create a Map
+  var map = new Map();
 
-        // Make map view and bind it to the map
-        var view = new SceneView({
-          container: "viewDiv",
-          map: map,
-          center: [-95.381764, 29.757776],
-          zoom: 12
-        });
+  // Make map view and bind it to the map
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    center: [-95.381764, 29.757776],
+    zoom: 12
+  });
 
-        var tileLayer = new VectorTileLayer({
-          url:
-            "https://tiles.arcgis.com/tiles/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Vector_Tile_Basemap/VectorTileServer"
-		});
-        map.add(tileLayer)
-      });
+  var tileLayer = new VectorTileLayer({
+    url:
+      "https://tiles.arcgis.com/tiles/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Vector_Tile_Basemap/VectorTileServer"
+  });
+  map.add(tileLayer);
+});
